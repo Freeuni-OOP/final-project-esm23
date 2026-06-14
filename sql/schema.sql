99ed597f7c3479257442c712a56f6d4d63a05670
Luca Gabadadze <gabadadzeluca@gmail.com>
2026-06-14 22:45:23 +0400

    feat: add quizzes table

--- sql/schema.sql
+++ sql/schema.sql
@@ -1,12 +1,24 @@
 DROP TABLE IF EXISTS users;
+DROP TABLE IF EXISTS quizes;
 
 CREATE TABLE users (
-   id            BIGINT AUTO_INCREMENT PRIMARY KEY,
-   username      VARCHAR(50)  NOT NULL UNIQUE,
-   password_hash VARCHAR(255) NOT NULL,
-   salt          VARCHAR(64)  NOT NULL,
-   is_admin      BOOLEAN      NOT NULL DEFAULT FALSE,
-   created_at    DATETIME     NOT NULL DEFAULT CURRENT_TIMESTAMP
+    id            BIGINT AUTO_INCREMENT PRIMARY KEY,
+    username      VARCHAR(50)  NOT NULL UNIQUE,
+    password_hash VARCHAR(255) NOT NULL,
+    salt          VARCHAR(64)  NOT NULL,
+    is_admin      BOOLEAN NOT NULL DEFAULT FALSE,
+    created_at    DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP
 );
 
-
+CREATE TABLE quizzes (
+    id BIGINT AUTO_INCREMENT PRIMARY KEY,
+    creator_id BIGINT NOT NULL,
+    title VARCHAR(150) NOT NULL,
+    description TEXT,
+    random_order BOOLEAN NOT NULL DEFAULT FALSE,
+    one_page BOOLEAN NOT NULL DEFAULT TRUE,
+    immediate_correction BOOLEAN NOT NULL DEFAULT FALSE,
+    practice_enabled BOOLEAN NOT NULL DEFAULT FALSE,
+    created_at DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    FOREIGN KEY (creator_id) REFERENCES users(id) ON DELETE CASCADE
+);
